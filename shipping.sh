0e0e46807e1c1601c00281ad50261fe47d02937f
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-26 04:32:48 +1200

    roboshopshell-script

--- shipping.sh
+++ shipping.sh
@@ -84,7 +84,7 @@ dnf install mysql -y &>> $LOGFILE
 
 VALIDATE $? "install MySQL client"
 
-mysql -h mysql.daws76s.online -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
+mysql -h 172.31.37.236 -uroot -pRoboShop@1 < /app/schema/shipping.sql &>> $LOGFILE
 
 VALIDATE $? "loading shipping data"
 
