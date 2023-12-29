6485d1d8bc24348947d51ee420cf64818cb89ad1
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-29 20:41:10 +1200

    shell-script

--- roboshop.sh
+++ roboshop.sh
@@ -1,19 +1,19 @@
-#!/bin/bash
+#!bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-013e4f22ecfb53901
-INSTANCES=("mongodb" "redis" "mysql" rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+SG_ID=sg-013e4f22ecfb53901 #replace with your SG ID
+INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+ZONE_ID=Z0391380A8B786GX6GIV
+DOMAIN_NAME="padimalayamuna.xyz"
 
-for i in "${ Instances[@] }"
-do    
-   if [ $i == "mongodb" ] || [ $i ==  "mysql"] || [ $i== "shipping" ]
-   then
-      INSTANCE_TYPE= "t3.small"
-   else
-      INSTANCE_TYPE= "t2.micro"
+for i in "${INSTANCES[@]}"
+do
+    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
+    then
+        INSTANCE_TYPE="t3.small"
+    else
+        INSTANCE_TYPE="t2.micro"
     fi
 
-    aws  ec2 run-instances --image-id ami-03265a0778a880afb --instance-type  t2.micro  --security-group-ids sg-013e4f22ecfb53901 
-
-done
+    IP_ADDRESS=$(aws ec2 run-instances -
 
