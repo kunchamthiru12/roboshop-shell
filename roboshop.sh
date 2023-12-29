c0d6f49bf843817a5a4319193f32549722360b16
kunchamthiru12 <thirumalakuncham123@gmail.com>
2023-12-29 19:56:10 +1200

    shell-script

--- roboshop.sh
+++ roboshop.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
 
 AMI=ami-03265a0778a880afb
-SG_ID=sg-013e4f22ecfb53901 #replace with your SG ID
-INSTANCES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
+SG_ID=sg-013e4f22ecfb53901
+INSTANCES=("mongodb" "redis" "mysql" rabbitmq" "catalogue" "user" "cart" "shipping" "payment"n"dispatch" "web")
 
-
-for i in "${INSTANCES[@]}"
-do
-    if [ $i == "mongodb" ] || [ $i == "mysql" ] || [ $i == "shipping" ]
-    then
-        INSTANCE_TYPE="t3.small"
-    else
-        INSTANCE_TYPE="t2.micro"
-    fi
-
-    IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type $INSTANCE_TYPE --security-group-ids sg-013e4f22ecfb53901--tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-    
-
-   
-
-done
+for i in "${ Instances[@]}"
+do    
+   if [ $i == "mongodb" ] || [ $i ==  "mysql"] || [ $i== "shipping" ]
+   then
+      INSTANCE_TYPE="t3.small"
+   else
+      INSTANCE_TYPE="t2.micro"
+done         
+aws  ec2 run-instances --image-id ami-03265a0778a880afb --instance-type  t2.micro  --se curity-group-ids sg-013e4f22ecfb53901 
+               
